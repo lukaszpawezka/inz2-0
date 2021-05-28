71fd3fb029032014f2ac4ad23f5d5513b40bdfd9
lpawezka <pawezkalukasz@gmail.com>
2021-05-28 18:18:31 +0200

    Init z backendem

---
+++ backend/sportapp-backend/src/main/java/sportapp/SportappApplication.java
@@ -0,0 +1,20 @@
+package sportapp;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SportappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportappApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
---
+++ backend/sportapp-backend/src/main/java/sportapp/cat/controller/CategoryController.java
@@ -0,0 +1,71 @@
+package sportapp.cat.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sportapp.cat.DTO.CategoryCreateDTO;
+import sportapp.cat.model.Category;
+import sportapp.cat.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+
+    @GetMapping()
+    public List<Category> getAllCategories(){
+        return categoryService.getAllCategories();
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public Category createCategory(@RequestBody CategoryCreateDTO categoryDTO) throws ParseException {
+        Category category = categoryService.convertToEntity(categoryDTO);
+        return categoryService.save(category);
+    }
+
+    @GetMapping("/category/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
+        Category category = categoryService.findCategoryById(id);
+        return ResponseEntity.ok(category);
+    }
+
+    @PutMapping("/category/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Long id, @RequestBody Category categoryDetails) {
+        Category category = categoryService.findCategoryById(id);
+        category.setName(categoryDetails.getName());
+        Category updatedCategory = categoryService.save(category);
+        return ResponseEntity.ok(updatedCategory);
+    }
+
+    @DeleteMapping("/category/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteCategory(@PathVariable Long id){
+        Category category = categoryService.findCategoryById(id);
+        categoryService.deleteCategory(category);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("Usunięto", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/main-categories")
+    public List<Category> getMainCategories(){
+        return categoryService.findMainCategories();
+    }
+
+    @GetMapping("/category-parent/{id}")
+    public ResponseEntity<Category> getCategoryParentById(@PathVariable Long id) {
+        Category category = categoryService.findCategoryById(id).getParent();
+        return ResponseEntity.ok(category);
+    }
+}
